5ea6f8e2f38aa95bba41e2a7bdbf7310ca5f6b7f
Zen <1984.apple@gmail.com>
2020-02-22 14:33:55 +0800

    new event: returnDraw, issue solved: MaxListeners change to Infinity.

--- index.js
+++ index.js
@@ -1,53 +1,40 @@
-const Room =  require('./classes/Room');
+const Room = require('./classes/Room');
 let roomInfo = new Room(1);
 
 const express = require('express');
 const app = express();
 const socketio = require('socket.io')
 
-// app.use(express.static(__dirname + '/public'));
-
 const expressServer = app.listen(3000);
 console.log('listening port 3000')
 const io = socketio(expressServer, {
-    path: '/socket.io',
-    serveClient: true
+  path: '/socket.io',
+  serveClient: true
 });
+io.on('connection', (socket) => {
+  console.log(`${socket.id} has join room ${roomInfo.roomId}`)
+  socket.setMaxListeners(Infinity); //new add
+  socket.on('joinRoom', (roomToJoin) => {
+    socket.join(roomToJoin);
 
-  io.on('connection', (socket) => {
-    console.log(`${socket.id} has join room ${roomInfo.roomId}`)
-    
-    socket.on('joinRoom', (roomToJoin) => {
-      socket.join(roomToJoin);
-      socket.emit('historyCatchUp', roomInfo.history)
-    })
-    // socket.emit('welcome', 'welcome to the websocket server!')
-    // socket.on('message', (msg) => {
-    //   console.log(msg)
-    //   socket.broadcast.emit('boardcast', msg)
-    // })
-    
-    setInterval(function() {
-    socket.on('newDraw', (draw) => {
-      roomInfo.addDrawing(draw)
-      console.log(roomInfo.history)
-      socket.broadcast.emit('showToClients', draw)
-      }, 33);
-    })
+  })
+  // test msg
+  // socket.emit('welcome', 'welcome to the websocket server!')
+  // socket.on('message', (msg) => {
+  //   console.log(msg)
+  //   socket.broadcast.emit('boardcast', msg)
+  // })
 
+  socket.on('newDraw', (draw) => {
+    console.log(roomInfo.history)
+    socket.broadcast.emit('showToClients', draw)
+  })
 
-    // //接受畫布作業訊息
-    // socket.on('test', function(data){
-    //   console.log(data);
-    //   //將畫布作業訊息傳給其他線上的人
-    //   socket.emit('fromServer', data);
-    // });
+  socket.on('returnDraw', (returnDraw) => {
+    socket.broadcast.emit('returnToClient', returnDraw)
+  })
 
-    // socket.on('ppap', (data)=> {
-    //   socket.broadcast.emit('fromServer', data);
-    // })
-
-    socket.on('disconnect', function(){
-      console.log('Client Disconnected');
-    });
+  socket.on('disconnect', function () {
+    console.log('Client Disconnected');
+  });
 })
